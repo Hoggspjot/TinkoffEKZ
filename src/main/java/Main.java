399125275ed541b07f8af65a6e2b6f7420281695
Hoggspjot <vingisaar@gmail.com>
2025-08-26 00:46:39 +0300

    five task correct

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,27 @@
 import java.util.*;
 
 public class Main {
+    private static final int MOD = 1000000007;
+
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-            int MOD = 1000000007;
+
+
             int n = scanner.nextInt();
             int k = scanner.nextInt();
 
-            long[][] dp = new long[n + 1][k + 1];
-            dp[0][0] = 1;
+            long[] dp = new long[n + 1];
+            dp[0] = 1;
 
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= Math.min(i, k); j++) {
-                    for (int x = 1; x <= i; x++) {
-                        if (j >= 1) {
-                            dp[i][j] = (dp[i][j] + dp[i - x][j - 1]) % MOD;
-                        }
+            for (int x = 1; x <= n; x++) {
+                for (int color = 0; color < k; color++) {
+                    for (int s = x; s <= n; s++) {
+                        dp[s] = (dp[s] + dp[s - x]) % MOD;
                     }
                 }
             }
 
-            long result = dp[n][k];
-            System.out.println(result);
+            System.out.println(dp[n]);
         }
     }
 }
