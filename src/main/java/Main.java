11dfa86ec517d15f9b29bbedd65b85f9b445f279
Hoggspjot <vingisaar@gmail.com>
2025-08-26 00:03:37 +0300

    five task

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,44 +2,26 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        long[] l = new long[n + 1];
-        long[] r = new long[n + 1];
-        long[] a = new long[n + 1];
-        for (int i = 1; i <= n; i++) {
-            l[i] = scanner.nextLong();
-            r[i] = scanner.nextLong();
-            a[i] = scanner.nextLong();
-        }
+        try (Scanner scanner = new Scanner(System.in)) {
+            int MOD = 1000000007;
+            int n = scanner.nextInt();
+            int k = scanner.nextInt();
 
-        long maxWater = 0;
-        for (int start = 1; start <= n; start++) {
-            long water = a[start];
-            long leftFlow = l[start];
-            long rightFlow = r[start];
+            long[][] dp = new long[n + 1][k + 1];
+            dp[0][0] = 1;
 
-
-            for (int i = start - 1; i >= 1; i--) {
-                long canFlow = Math.min(leftFlow, a[i]);
-                water += canFlow;
-                leftFlow = Math.min(leftFlow - canFlow, l[i]);
-                if (leftFlow == 0)
-                    break;
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= Math.min(i, k); j++) {
+                    for (int x = 1; x <= i; x++) {
+                        if (j >= 1) {
+                            dp[i][j] = (dp[i][j] + dp[i - x][j - 1]) % MOD;
+                        }
+                    }
+                }
             }
 
-
-            for (int i = start + 1; i <= n; i++) {
-                long canFlow = Math.min(rightFlow, a[i]);
-                water += canFlow;
-                rightFlow = Math.min(rightFlow - canFlow, r[i]);
-                if (rightFlow == 0)
-                    break;
-            }
-
-            maxWater = Math.max(maxWater, water);
+            long result = dp[n][k];
+            System.out.println(result);
         }
-
-        System.out.println(maxWater);
     }
 }
