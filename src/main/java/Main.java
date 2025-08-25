07eccf16a863ebf2f13006a13c1c891478bf057c
Hoggspjot <vingisaar@gmail.com>
2025-08-25 23:55:21 +0300

    four task correct close res

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,44 +2,46 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        long[] l = new long[n + 1];
-        long[] r = new long[n + 1];
-        long[] a = new long[n + 1];
-        for (int i = 1; i <= n; i++) {
-            l[i] = scanner.nextLong();
-            r[i] = scanner.nextLong();
-            a[i] = scanner.nextLong();
-        }
+        try (Scanner scanner = new Scanner(System.in)) {
 
-        long maxWater = 0;
-        for (int start = 1; start <= n; start++) {
-            long water = a[start];
-            long leftFlow = l[start];
-            long rightFlow = r[start];
-
-
-            for (int i = start - 1; i >= 1; i--) {
-                long canFlow = Math.min(leftFlow, a[i]);
-                water += canFlow;
-                leftFlow = Math.min(leftFlow - canFlow, l[i]);
-                if (leftFlow == 0)
-                    break;
+            int n = scanner.nextInt();
+            long[] l = new long[n + 1];
+            long[] r = new long[n + 1];
+            long[] a = new long[n + 1];
+            for (int i = 1; i <= n; i++) {
+                l[i] = scanner.nextLong();
+                r[i] = scanner.nextLong();
+                a[i] = scanner.nextLong();
             }
 
+            long maxWater = 0;
+            for (int start = 1; start <= n; start++) {
+                long water = a[start];
+                long leftFlow = l[start];
+                long rightFlow = r[start];
 
-            for (int i = start + 1; i <= n; i++) {
-                long canFlow = Math.min(rightFlow, a[i]);
-                water += canFlow;
-                rightFlow = Math.min(rightFlow - canFlow, r[i]);
-                if (rightFlow == 0)
-                    break;
+
+                for (int i = start - 1; i >= 1; i--) {
+                    long canFlow = Math.min(leftFlow, a[i]);
+                    water += canFlow;
+                    leftFlow = Math.min(leftFlow - canFlow, l[i]);
+                    if (leftFlow == 0)
+                        break;
+                }
+
+
+                for (int i = start + 1; i <= n; i++) {
+                    long canFlow = Math.min(rightFlow, a[i]);
+                    water += canFlow;
+                    rightFlow = Math.min(rightFlow - canFlow, r[i]);
+                    if (rightFlow == 0)
+                        break;
+                }
+
+                maxWater = Math.max(maxWater, water);
             }
 
-            maxWater = Math.max(maxWater, water);
+            System.out.println(maxWater);
         }
-
-        System.out.println(maxWater);
     }
 }
